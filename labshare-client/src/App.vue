27707a567e966882d63880e271fc3be04782d309
Fabian Gassen <fabian.gassen@me.com>
2020-03-21 23:03:30 +0100

    [labshare-client] App global styles and move to scss

--- labshare-client/src/App.vue
+++ labshare-client/src/App.vue
@@ -1,10 +1,8 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="Vue logo" src="./assets/logo.png" />
     <h1>App</h1>
-    <router-view>
-
-    </router-view>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -19,14 +17,12 @@ import a from './lib/validation'
 a.validateFirstname("test")
 
 export default {
-  name: 'App',
-  components: {
-
-  }
-}
+  name: "App",
+  components: {}
+};
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -34,5 +30,42 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.img-fluid {
+  max-width: 100%;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.checkbox-group {
+  &.checkbox--inline {
+    display: flex;
+    flex-flow: row wrap;
+
+    .checkbox:not(:last-child) {
+      margin-right: 1rem;
+    }
+  }
+}
+
+.checkbox {
+  display: flex;
+  justify-content: flex-start;
+}
+
+.p-dropdown {
+  width: 100%;
+  text-align: left;
+}
+
+.form-element-spacer {
+  margin-bottom: 2rem;
+}
+
+.p-float-label {
+  input {
+    width: 100%;
+  }
 }
 </style>
