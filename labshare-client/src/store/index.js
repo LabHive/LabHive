34f008dc00524621cefc4427fac5a7632f09865c
Fabian Gassen <fabian.gassen@me.com>
2020-03-21 23:04:11 +0100

    [labshare-client] updated store

--- labshare-client/src/store/index.js
+++ labshare-client/src/store/index.js
@@ -5,10 +5,50 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    session: null,
+    isAuthenticated: false,
+    listData: null
   },
   mutations: {
+    setSession (state, payload) {
+      state.session = payload
+    },
+    login (state) {
+      state.isAuthenticated = true;
+    },
+    logout (state) {
+      state.isAuthenticated = false;
+    }
+  },
+  getters: {
+    session: state => {
+      return state.session
+    },
+    listData: state => {
+      return state.listData
+    },
+    authenticated: state => {
+      return state.isAuthenticated
+    }
   },
   actions: {
+    login ({ commit }, payload) {
+      commit('setSession', payload);
+      commit('login');
+    },
+    logout ({ commit }, payload) {
+      commit('setSession', payload);
+      commit('logout');
+    },
+    getListData({ Vue }) {
+      Vue.$http.get('profile')
+      .then(success => {
+        console.log(success.body)
+      },
+      error => {
+        console.log(error)
+      })
+    }
   },
   modules: {
   }
