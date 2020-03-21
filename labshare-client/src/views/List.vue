1bd0c8f6680fd8b2f28356c158137192569752a7
Fabian Gassen <fabian.gassen@me.com>
2020-03-21 23:02:38 +0100

    [labshare-client] List component for rendering results from BE

---
+++ labshare-client/src/views/List.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="list-view">
+    <h1>List</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "List",
+  props: {},
+  computed: {
+      data: function() {
+          return this.$store.getters.listData
+      }
+  },
+  data: function() {
+    return {
+
+    };
+  },
+  components: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
