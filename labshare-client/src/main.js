c0e5382a2c0d424ae4d81da945b3ec756dce762e
Fabian Gassen <fabian.gassen@me.com>
2020-03-21 23:03:57 +0100

    [labshare-client] add list route and restrict access to it

--- labshare-client/src/main.js
+++ labshare-client/src/main.js
@@ -9,16 +9,27 @@ import App from './App.vue'
 import Index from './views/Index'
 import Login from './views/Login'
 import Register from './views/Register'
+import List from './views/List'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter);
 Vue.use(VueResource);
 Vue.use(Vuex);
+Vue.http.options.root = 'http://0.0.0.0:5000/api/v1';
+
+const ifAuthenticated = (to, from, next) => {
+  if (store.getters.isAuthenticated) {
+    next()
+    return
+  }
+  next('/')
+}
 
 const routes = [
   { path: '/', component: Index },
   { path: '/login', component: Login },
-  { path: '/register', component: Register}
+  { path: '/register', component: Register},
+  { path: '/list', component: List, beforeEnter: ifAuthenticated }
 ];
 
 const router = new VueRouter({ routes });
