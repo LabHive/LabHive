fd9fd4af4c323102fb3e9ed446b6a0d9974f4788
FredericNk <frederic.weichel@gmail.com>
2020-03-22 14:32:45 +0100

    Changed devpost Text, expanded überuns Text

--- labshare-client/src/views/UeberUns.vue
+++ labshare-client/src/views/UeberUns.vue
@@ -11,9 +11,15 @@
             <lI>Labore mit Laboren zum Austausch von Geräten</li>
             <li>Labore mit Wissenschaftlern mit spezialisiertem Know-How </li>
           </ul>
-        <hr class="my-4">
+          <hr class="my-4">
           </div>
-
+        </div>
+        <div class="row">
+          <div class="col-sm">
+            <p>Im Frühjahr 2020 breitete sich das neue SARS-CoV-2 Virus über die Welt aus. Die durch eine Infektion ausgelöste Krankheit COVID-19 zeigt eine sehr hohe Ausbreitungsrate bei Vergleichsweise schweren und langen Verlauf. Hierdurch besteht die Gefahr, dass auch in entwickelten Ländern Gesundheitssysteme unter der massiven Anzahl an Erkrankten zusammenbrechen, was zu einer erhöhten Mortalität führt. 
+Patienten sind häufig bereits vor dem Auftreten von Symptomen infektiös, was zu der Beobachteten hohen Ausbreitungsrate beiträgt. Um die Ausbreitung in der Bevölkerung zu verfolgen, infizierte zu quarantänisieren und bei abklingen der Infektion aus der quarantäne zu entlassen werden RT-PCR Tests eingesetzt um das Virus nachzuweisen. Um zusätzliche Ressourcen von Menschen mit benötigtem Know-How und stillstehende Geräte aus geschlossenen Labors zu aktivieren erleichtern wir die Kommunikation zwischen engagierten Menschen und der Diagnostik und helfen somit dabei die Infektionswelle bestmöglich zu bewältigen.
+</p>
+          </div>
         </div>
       </div>
 
