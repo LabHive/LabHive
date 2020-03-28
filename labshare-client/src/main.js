b0e53d3d110105213e7a30fe8bc86971287606c9
Seth Greylyn <9030612+SethGreylyn@users.noreply.github.com>
2020-03-28 20:52:15 +0100

    Dependencies and proof of concept for single-file localization

--- labshare-client/src/main.js
+++ labshare-client/src/main.js
@@ -1,23 +1,26 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import router from './router.js'
-import store from './store'
-import App from './App.vue'
-import './http'
-import profile_load_mixin from './mixins/profile_fetch'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import Vue from "vue";
+import Vuex from "vuex";
+import router from "./router.js";
+import store from "./store";
+import App from "./App.vue";
+import "./http";
+import profile_load_mixin from "./mixins/profile_fetch";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import VueI18n from "vue-i18n";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 // Install BootstrapVue
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
 // Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
+Vue.use(IconsPlugin);
 Vue.use(Vuex);
 Vue.mixin(profile_load_mixin);
+Vue.use(VueI18n);
 
 new Vue({
   router,
   store,
+  i18n: new VueI18n({ locale: "de" }),
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
--- labshare-client/vue.config.js
+++ labshare-client/vue.config.js
@@ -1,13 +1,22 @@
 var path = require("path");
 
 module.exports = {
-    configureWebpack: {
-      resolve: {
-        alias: {
-            'assets': path.resolve(__dirname, 'src/assets'),
-            'components': path.resolve(__dirname, 'src/components'),
-            'views': path.resolve(__dirname, 'src/views')
-        }
+  chainWebpack: config => {
+    config.module
+      .rule("i18n")
+      .resourceQuery(/blockType=i18n/)
+      .type("javascript/auto")
+      .use("i18n")
+      .loader("@kazupon/vue-i18n-loader")
+      .end();
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        assets: path.resolve(__dirname, "src/assets"),
+        components: path.resolve(__dirname, "src/components"),
+        views: path.resolve(__dirname, "src/views")
       }
     }
   }
+};
