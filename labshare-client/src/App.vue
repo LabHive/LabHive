efb9d4bb7bc19abe95147c40fb3e465906ae2201
Seth Greylyn <9030612+SethGreylyn@users.noreply.github.com>
2020-03-29 13:10:52 +0200

    Top-level app localization

--- labshare-client/src/App.vue
+++ labshare-client/src/App.vue
@@ -1,25 +1,38 @@
+<i18n>
+{
+  "en": {},
+  "de": {
+    "brand": "LabHive",
+    "aboutUs": "Über uns",
+    "register": "Registrieren",
+    "login": "Login",
+    "profile": "Profile",
+    "signOut": "Sign Out"
+  }
+}
+</i18n>
 <template>
   <div class="main-div">
     <b-navbar toggleable="lg" type="dark" variant="primary" id="navbar">
       <div class="nav-container">
-        <b-navbar-brand href="#/">LabHive</b-navbar-brand>
+        <b-navbar-brand href="#/">{{$t('brand')}}</b-navbar-brand>
         <b-navbar-toggle style="float: right" target="nav-collapse"></b-navbar-toggle>
 
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav>
-            <b-nav-item href="#/UeberUns">Über uns</b-nav-item>
+            <b-nav-item href="#/UeberUns">{{$t('aboutUs')}}</b-nav-item>
           </b-navbar-nav>
 
           <!-- Right aligned nav items -->
           <b-navbar-nav class="ml-auto">
             <template v-if="!$authenticated">
-              <b-nav-item href="#/register">Registrieren</b-nav-item>
-              <b-nav-item href="#/login">Login</b-nav-item>
+              <b-nav-item href="#/register">{{$t('register')}}</b-nav-item>
+              <b-nav-item href="#/login">{{$t('login')}}</b-nav-item>
             </template>
 
             <b-nav-item-dropdown v-if="$authenticated" right :text="userName">
-              <b-dropdown-item href="#">Profile</b-dropdown-item>
-              <b-dropdown-item href="#" @click="logout">Sign Out</b-dropdown-item>
+              <b-dropdown-item href="#">{{$t('profile')}}</b-dropdown-item>
+              <b-dropdown-item href="#" @click="logout">{{$t('signOut')}}</b-dropdown-item>
             </b-nav-item-dropdown>
             <LocaleChange />
           </b-navbar-nav>
