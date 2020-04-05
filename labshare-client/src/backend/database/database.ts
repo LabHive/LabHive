be26d28cc8a7a6e2767b1d5c591e9f7b7c90e2c7
Seth Greylyn <9030612+SethGreylyn@users.noreply.github.com>
2020-04-05 10:39:13 +0200

    Some formatting noise from my VSCode, simple open query for lab locations, simple echo endpoint

--- labshare-client/src/backend/database/database.ts
+++ labshare-client/src/backend/database/database.ts
@@ -1,71 +1,102 @@
-import mongoose, { Model } from "mongoose"
-import { IUserVolunteer, UserVolunteerSchema } from './schemas/IUserVolunteer'
-import { IUserLabDiag, UserLabDiagSchema } from './schemas/IUserLabDiag'
-import { IResetToken, ResetTokenSchema } from './schemas/IResetToken'
-import { IUserCommon } from './schemas/IUserCommon'
-import { UserLabResearchSchema, IUserLabResearch } from './schemas/IUserLabResearch'
-import { UserRoles } from '../../lib/userRoles'
+import mongoose, { Model } from 'mongoose';
+import { IUserVolunteer, UserVolunteerSchema } from './schemas/IUserVolunteer';
+import { IUserLabDiag, UserLabDiagSchema } from './schemas/IUserLabDiag';
+import { IResetToken, ResetTokenSchema } from './schemas/IResetToken';
+import { IUserCommon } from './schemas/IUserCommon';
+import {
+  UserLabResearchSchema,
+  IUserLabResearch,
+} from './schemas/IUserLabResearch';
+import { UserRoles } from '../../lib/userRoles';
 
 if (!process.env.PRODUCTION) {
-    mongoose.connect("mongodb://localhost:27017/labshare", { useNewUrlParser: true }, err => {
-        console.log(err)
-    })
+  mongoose.connect(
+    'mongodb://localhost:27017/labshare',
+    { useNewUrlParser: true },
+    (err) => {
+      console.log(err);
+    }
+  );
 } else {
-    mongoose.connect("mongodb://mongodb:27017/labshare", { useNewUrlParser: true }, err => {
-        console.log(err)
-    })
+  mongoose.connect(
+    'mongodb://mongodb:27017/labshare',
+    { useNewUrlParser: true },
+    (err) => {
+      console.log(err);
+    }
+  );
 }
 
-
-export const UserVolunteer = mongoose.model<IUserVolunteer>('user_volunteer', UserVolunteerSchema)
-export const UserLabDiag = mongoose.model<IUserLabDiag>('user_labDiag', UserLabDiagSchema)
-export const UserLabResearch = mongoose.model<IUserLabResearch>('user_labResearch', UserLabResearchSchema)
-export const ResetToken = mongoose.model<IResetToken>('reset_token', ResetTokenSchema)
-
+export const UserVolunteer = mongoose.model<IUserVolunteer>(
+  'user_volunteer',
+  UserVolunteerSchema
+);
+export const UserLabDiag = mongoose.model<IUserLabDiag>(
+  'user_labDiag',
+  UserLabDiagSchema
+);
+export const UserLabResearch = mongoose.model<IUserLabResearch>(
+  'user_labResearch',
+  UserLabResearchSchema
+);
+export const ResetToken = mongoose.model<IResetToken>(
+  'reset_token',
+  ResetTokenSchema
+);
 
 export function getUserForMail(email: string): Promise<Optional<IUserCommon>> {
-    return getUser({ "contact.email": email })
+  return getUser({ 'contact.email': email });
 }
 
 export function getUserById(id: string): Promise<Optional<IUserCommon>> {
-    return getUser({_id: id})
+  return getUser({ _id: id });
 }
 
 export function getModelForRole(role: string): Optional<Model<IUserCommon>> {
-    switch (role) {
-        case UserRoles.LAB_DIAG:
-            return UserLabDiag
-        case UserRoles.LAB_RESEARCH:
-            return UserLabResearch
-        case UserRoles.VOLUNTEER:
-            return UserVolunteer;
-        default:
-            console.log("Cannot get database model for role " + role)
-            return undefined
-    }
+  switch (role) {
+    case UserRoles.LAB_DIAG:
+      return UserLabDiag;
+    case UserRoles.LAB_RESEARCH:
+      return UserLabResearch;
+    case UserRoles.VOLUNTEER:
+      return UserVolunteer;
+    default:
+      console.log('Cannot get database model for role ' + role);
+      return undefined;
+  }
 }
 
 export async function getUser(filter: any): Promise<Optional<IUserCommon>> {
-    let models: Model<any>[] = [UserLabResearch, UserLabDiag, UserVolunteer]
-    let matches = []
+  let models: Model<any>[] = [UserLabResearch, UserLabDiag, UserVolunteer];
+  let matches = [];
 
-    for (let i of models) {
-        try {
-            let match = await i.findOne(filter).exec()
-            if (match) {
-                matches.push(match)
-            }
-        } catch {
-            return undefined
-        }
+  for (let i of models) {
+    try {
+      let match = await i.findOne(filter).exec();
+      if (match) {
+        matches.push(match);
+      }
+    } catch {
+      return undefined;
     }
+  }
 
-    if (matches.length > 1) {
-        throw new Error("More than one result found.")
-    } else if (matches.length == 0) {
-        return undefined
-    } else {
-        return matches[0]
-    }
+  if (matches.length > 1) {
+    throw new Error('More than one result found.');
+  } else if (matches.length == 0) {
+    return undefined;
+  } else {
+    return matches[0];
+  }
 }
 
+export async function getLabLocations(): Promise<
+  Array<{ lat: number; long: number }>
+> {
+  const labDiags = await UserLabDiag.find();
+
+  return labDiags.map((lab) => ({
+    lat: lab.location.coordinates[1],
+    long: lab.location.coordinates[0],
+  }));
+}
--- labshare-client/src/backend/main.ts
+++ labshare-client/src/backend/main.ts
@@ -1,51 +1,50 @@
-import cors from 'cors'
-import express, { response } from "express"
-import { readFileSync } from 'fs'
-import { registration } from './endpoints/registration'
-import { forgotPassword } from './endpoints/forgotPassword'
-import { resetPassword } from './endpoints/resetPassword'
-import { login } from './endpoints/login'
-import { authMiddleware } from './middlewares/auth'
-import { changePassword } from './endpoints/changePassword'
-import { search } from "./endpoints/search";
-import Profile from './endpoints/Profile'
-import { language } from './endpoints/language'
+import cors from 'cors';
+import express, { response } from 'express';
+import { readFileSync } from 'fs';
+import { registration } from './endpoints/registration';
+import { forgotPassword } from './endpoints/forgotPassword';
+import { resetPassword } from './endpoints/resetPassword';
+import { login } from './endpoints/login';
+import { authMiddleware } from './middlewares/auth';
+import { changePassword } from './endpoints/changePassword';
+import { search } from './endpoints/search';
+import Profile from './endpoints/Profile';
+import { language } from './endpoints/language';
+import { labLocations } from './endpoints/labLocations';
 
-let app = express()
-let router = express.Router()
+let app = express();
+let router = express.Router();
 
-export let HMAC_KEY: string
+export let HMAC_KEY: string;
 if (process.env.PRODUCTION) {
-    app.use(express.static('dist'));
-    HMAC_KEY = readFileSync('./secret/jsonwebtoken_hmacKey.txt', { encoding: 'utf8' })
-}
-else {
-    app.use(cors())
-    HMAC_KEY = "randomHmacKey"
+  app.use(express.static('dist'));
+  HMAC_KEY = readFileSync('./secret/jsonwebtoken_hmacKey.txt', {
+    encoding: 'utf8',
+  });
+} else {
+  app.use(cors());
+  HMAC_KEY = 'randomHmacKey';
 }
 
 app.use(express.json());
-app.use('/api/v1', router)
+app.use('/api/v1', router);
 
-router.get('/language', language)
-router.post('/registration', registration)
-router.post('/forgot-password', forgotPassword)
-router.post("/reset-password", resetPassword)
-router.post("/login", login)
-router.get("/search", search)
+router.get('/language', language);
+router.post('/registration', registration);
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password', resetPassword);
+router.post('/login', login);
+router.get('/search', search);
+router.get('/labLocations', labLocations);
 
+router.use(authMiddleware);
 
-router.use(authMiddleware)
-
-router.post("/change-password", changePassword)
-router.get("/profile", Profile.get)
-    .post("/profile", Profile.post)
-    .delete("/profile", Profile.delete)
-
+router.post('/change-password', changePassword);
+router
+  .get('/profile', Profile.get)
+  .post('/profile', Profile.post)
+  .delete('/profile', Profile.delete);
 
 app.listen(5000, function () {
-    console.log('Example app listening on port 5000!')
-})
-
-
-
+  console.log('Example app listening on port 5000!');
+});
