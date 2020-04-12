f9cf6efa48fb021d2ff9a49d26aa4e65359f03e0
Philipp Nieting <philipp@nieting.de>
2020-04-12 23:04:55 +0200

    Wrapped test-coverage endpoint in try/catch

--- labshare-client/src/backend/endpoints/testCoverage.ts
+++ labshare-client/src/backend/endpoints/testCoverage.ts
@@ -1,12 +1,18 @@
 import express from 'express';
 import { getTestCoverage } from '../database/database';
+import utils from '../utils';
 
 export async function testCoverage(
   req: express.Request,
   res: express.Response
 ) {
-  res.send({
-    success: true,
-    data: await getTestCoverage()
-  });
+  try {
+    res.send({
+      success: true,
+      data: await getTestCoverage()
+    });
+  }
+  catch(err) {
+    utils.internalError(res)
+  }
 }
