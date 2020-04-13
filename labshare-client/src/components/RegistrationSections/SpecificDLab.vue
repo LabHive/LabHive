31150b200a41fd66d9ebd54bc6f667321e9a9449
Philipp Nieting <philipp@nieting.de>
2020-04-13 21:30:49 +0200

    Added information about the system to labs registration forms

--- labshare-client/src/components/RegistrationSections/SpecificDLab.vue
+++ labshare-client/src/components/RegistrationSections/SpecificDLab.vue
@@ -3,13 +3,14 @@
     "en":{
     "section": "Further Information",
     "furtherInfo": "Further Information",
-    "optIn": "Optional Consent"
+    "optIn": "Optional Consent",
+    "dataProcessingInformation": "By finishing the registration your data will be processed by LabHive. Your specified contact information is only visible for registered and internally verified research and diagnostic laboratories. Currently, registered volunteers have no possibility to respond to requests from diagnostic laboratories to prevent an increased E-mail flow. We are working on a solution that solves this problem for both sides. As a diagnostic laboratory you can however find volunteers through the search function, view their contact data and contact them.<br/>Further information can be found in our <a href=\"/#/privacyPolicy\">privacy policy</a>."
     },
     "de":{
     "section": "Weitere Informationen",
     "furtherInfo": "Weitere Informationen",
     "optIn": "Optionale Einwilligung",
-    "dataProcessingInformation": "<p>Durch das Abschließen der Registrierung werden Ihre Daten von LabHive verarbeitet. Ihre angegebenen Kontaktinformationen sind nur für registrierte und von uns verifizierte Forschungs- und Diagnostiklabore sichtbar. Registrierte Helfer haben momentan noch keine Möglichkeit auf ihre Suchanfragen zu reagieren. Sie können allerdings Helfer über die Suchfunktion finden, deren Kontaktdaten sehen und diese kontaktieren.<br/>Weiter Informationen finden Sie in unserer Datenschutzerklärung.</p>"
+    "dataProcessingInformation": "<p>Durch das Abschließen der Registrierung werden Ihre Daten von LabHive verarbeitet. Ihre angegebenen Kontaktinformationen sind nur für registrierte und von uns verifizierte Forschungs- und Diagnostiklabore sichtbar. Registrierte Helfer*innen haben momentan noch keine Möglichkeit auf Suchanfragen von Seiten der Diagnostiklabore zu reagieren, um ein erhöhtes E-Mail Aufkommen für diese zu vermeiden. Wir arbeiten jedoch an einer Lösung für beide Seiten. Als Diagnostiklabor können Sie allerdings Helfer*innen über die Suchfunktion finden, deren Kontaktdaten sehen und diese kontaktieren.<br/>Weitere Informationen finden Sie in unserer <a href=\"/#/privacyPolicy\">Datenschutzerklärung</a>.</p>"
     }
     }
 </i18n>
@@ -37,7 +38,7 @@
       >{{ $t('not_agree') }}</b-form-radio>
     </b-form-group>
 
-    <div>{{ $t("dataProcessingInformation") }}</div>
+    <div v-html="$t('dataProcessingInformation')"></div>
 
 
     <h3 class="section">{{$t("section")}}</h3>
