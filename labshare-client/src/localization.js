4858a0c4f5c4185c21efd455188ef3ea8452d139
Philipp Nieting <philipp@nieting.de>
2020-04-14 23:22:51 +0200

    Added templates

--- labshare-client/src/localization.js
+++ labshare-client/src/localization.js
@@ -9,18 +9,9 @@ export let localization = {
   silentFallbackWarn: true,
   messages: {
     en: {
-      required: "Required",
-      consentMail: "I agree that LabHive sends me from time to time E-Mail-Updates about new features of the platform.",
-      agree: "I agree.",
-      not_agree: "I don't agree.",
-      deleteProfile: "Delete Profile",
-      delete: "Delete",
-      cancel: "Cancel",
-      deleteConfirmation: "Do you really want to delete your profile?"
-    },
-    de: {
       backend: {
         formValidation: {
+          // english should go below here
           invalid_email: "Ungültige E-Mail",
           invalid_firstname: "Ungültiger Vorname (max. 200 Zeichen)",
           invalid_lastname: "Ungültiger Nachname (max. 200 Zeichen)",
@@ -62,9 +53,56 @@ export let localization = {
       agree: "Ich willige ein.",
       not_agree: "Ich willige nicht ein.",
       qualification: "Qualifikation",
-      skills: {
-        bsl2: "BSL2 Erfahrung"
+      deleteProfile: "Profil löschen",
+      delete: "Löschen",
+      cancel: "Abbruch",
+      deleteConfirmation: "Wollen Sie ihr Profil wirklich löschen?"
+    },
+    de: {
+      backend: {
+        formValidation: {
+          // german translation, no action needed here.
+          invalid_email: "Ungültige E-Mail",
+          invalid_firstname: "Ungültiger Vorname (max. 200 Zeichen)",
+          invalid_lastname: "Ungültiger Nachname (max. 200 Zeichen)",
+          invalid_zipcode: "Ungültige PLZ",
+          invalid_city: "Ungültige Stadt (max. 200 Zeichen)",
+          invalid_street: "Ungültige Straße (max. 200 Zeichen)",
+          invalid_phone: "Ungültige Telefonnummer bitte in der Form +49931123 oder 12344 eingeben ohne - oder /",
+          invalid_description: "Ungültige Beschreibung (max. 2000 Zeichen)",
+          invalid_name: "Ungültiger Laborname (max. 200 Zeichen)",
+          invalid_number: "Ungültige Zahl",
+          invalid_search: "Ungültiger Suchparameter",
+          invalid_role: "Ungültiger Suchtyp",
+          invalid_skills: "Ungültige Fähigkeiten",
+          invalid_equipment: "Ungültiges Equipment",
+          invalid_advice: "Ungültige Anfrage",
+          invalid_consent: "Ungültige Einwilligung",
+          invalid_url: "Ungültige URL, muss mit http:// oder https:// beginnen.",
+          passwordMatch: "Passwörter stimmen nicht überein!"
+        },
+        not_authorized: "Nicht authorisiert!",
+        invalid_location: "Ungültige Adresse!",
+        internal_error: "Es ist etwas schiefgelaufen...",
+        bad_request: "Ungültige Anfrage!",
+        invalid_password: "Ungültiges Passwort!",
+        invalid_login: "Ungültige Login-Daten!",
+        existing_user: "User existiert bereits!",
+        false_consent: "Ohne Einwilligung dürfen wir die Daten nicht verarbeiten.",
+        no_results: "No results could be found",
+        tokenTooOld: "Bitte setzen Sie das Passwort erneut zurück, der Link ist abgelaufen.",
+        contact_support: "Ein Fehler ist aufgetreten, bitte wenden Sie sich an support@labhive.de. Bitte entschuldigen Sie die Umstände!",
+        user_not_activated: "Der Benutzer ist noch nicht aktiviert. Zur Aktivierung Ihres Accounts müssen Sie die E-Mail Adresse bestätigen. Sollten Sie sich als Labor registriert haben, werden Sie von uns manuell verifiziert. Sobald dies geschehen ist, erhalten Sie von uns eine E-Mail. Sollte es länger als 2 Werktage dauern, kontaktieren Sie uns bitte unter support@labhive.de.",
+        user_disabled: "Ihr Account wurde von uns deaktiviert. Bei Fragen wenden Sie sich bitte an support@labhive.de."
       },
+      save: "Speichern",
+      consentPublicSearch:
+        "Ich bin damit einverstanden, dass meine Daten von LabHive zum Zwecke der Zusammenarbeit mit Laboren und Instituten, im Rahmen der SARS-CoV-2 Pandemie, die auf der Plattform registriert sind, über die Suchfunktion einsehbar gemacht werden. Das Recht zum Widerruf und meine Rechte gemäß Art. 15 DSGVO sind von der Einwilligung unberührt.<br/>Wenn Sie uns keine Einwilligung erteilen, ist die Verwendung unserer Plattform nur ehr eingeschränkt möglich, wodurch Sie als Helfende*r nicht über die Suche gefunden werden können. Momentan gibt es auch noch keine Möglichkeit Kontakt zu einem registrierten und verifizierten Labor aufzunehmen.<br/>Weitere Hinweise finden Sie in unserer <a href=\"/#/privacyPolicy\">Datenschutzerklärung.</a>",
+      consentMail: "Hiermit willige ich ein, dass LabHive mir gelegentlich E-Mail-Benachrichtigungen über Updates und neue Funktionen der Website schickt.",
+      required: "Erforderlich",
+      agree: "Ich willige ein.",
+      not_agree: "Ich willige nicht ein.",
+      qualification: "Qualifikation",
       deleteProfile: "Profil löschen",
       delete: "Löschen",
       cancel: "Abbruch",
