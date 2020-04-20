fa70d41f39e371a71fbc51e2ccd372640fd67c72
frwms <frederic.schade@gmail.com>
2020-04-20 20:39:24 +0200

    Fine-tuned coverage map look, added hover effect

--- labshare-client/src/components/Coverage.vue
+++ labshare-client/src/components/Coverage.vue
@@ -243,4 +243,16 @@ $color-bkg-primary: #f7f6fd;
     }
   }
 }
+
+.vue2leaflet-map {
+  border-radius: 8px;
+  box-shadow: 0 4px 16px rgba(0,0,0,0.15);
+  transition: all 0.15s ease-in-out;
+
+  &:hover {
+    box-shadow: 0 8px 20px rgba(0,0,0,0.20);
+    transform: scale(1.01);
+    transition: all 0.15s ease-in-out;
+  }
+}
 </style>
