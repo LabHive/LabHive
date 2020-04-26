211cb8304d930496439b4e94de4a704fe4ceed3b
Philipp Nieting <philipp@nieting.de>
2020-04-26 18:40:42 +0200

    Fixed Checkbox Group column spacing

--- labshare-client/src/components/CheckboxGroup.vue
+++ labshare-client/src/components/CheckboxGroup.vue
@@ -3,7 +3,7 @@
     <b-container>
       <b-row>
         <template v-for="i in cols_number">
-          <b-col cols lg="6" md="6" sm="12" style="padding: 0" :key="i">
+          <b-col sm :key="i" md="auto">
             <b-form-checkbox-group
               stacked
               :id="name + i"
