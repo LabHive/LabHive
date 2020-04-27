41f254b1be885bbfe443562fee1766a1e911ec8d
Philipp Nieting <philipp@nieting.de>
2020-04-27 23:47:26 +0200

    Added confirmation modal for resetting registration form

--- labshare-client/src/components/RegistrationProgress.vue
+++ labshare-client/src/components/RegistrationProgress.vue
@@ -9,7 +9,11 @@
     "consent": "Optional Consents",
     "affiliation_contact": "Affiliation & Contact",
     "request": "Current Requests",
-    "offer": "Current Offers"
+    "offer": "Current Offers",
+    "reset": "Attention!",
+    "warning": "The entered data is going to be cleaned. Do you want to continue?",
+    "yes": "Yes",
+    "no": "No"
   },
   "de": {
     "usergroup": "Nutzergruppe",
@@ -20,15 +24,29 @@
     "consent": "Optionale Einwilligung",
     "affiliation_contact": "Affiliation & Kontaktperson",
     "request": "Aktueller Bedarf",
-    "offer": "Aktuelles Angebot"
+    "offer": "Aktuelles Angebot",
+    "reset": "Achtung!",
+    "warning": "Es werden alle Daten zurückgesetzt. Wollen sie wirklich fortfahren?",
+    "yes": "Ja",
+    "no": "Nein"
   }
 }
 </i18n>
 <template>
   <div>
+    <b-modal
+      id="confirm"
+      :title="$t('attention')"
+      :ok-title="$t('yes')"
+      :cancel-title="$t('no')"
+      ok-variant="primary"
+      @ok="setStep(0)"
+    >
+      <p class="my-4">{{ $t('warning') }}</p>
+    </b-modal>
     <b-container>
       <b-row v-if="role === 'volunteer'">
-        <b-col cols="auto"><b-button variant="primary" :class="classForStep(0)" @click="setStep(0)">1: {{ $t("usergroup") }}</b-button></b-col>
+        <b-col cols="auto"><b-button variant="primary" :class="classForStep(0)" v-b-modal.confirm>1: {{ $t("usergroup") }}</b-button></b-col>
         <b-col cols="auto"><b-button variant="primary" :class="classForStep(1)" @click="setStep(1)">2: {{ $t("loginInformation") }}</b-button></b-col>
         <b-col cols="auto"><b-button variant="primary" :class="classForStep(2)" @click="setStep(2)">3: {{ $t("qualification") }}</b-button></b-col>
         <b-col cols="auto"><b-button variant="primary" :class="classForStep(3)" @click="setStep(3)">4: {{ $t("skills") }}</b-button></b-col>
@@ -36,7 +54,7 @@
         <b-col cols="auto"><b-button variant="primary" :class="classForStep(5)" @click="setStep(5)">6: {{ $t("consent") }}</b-button></b-col>
       </b-row>
       <b-row v-else-if="role === 'lab_diag'">
-        <b-col cols="auto"><b-button variant="primary" :class="classForStep(0)" @click="setStep(0)">1: {{ $t("usergroup") }}</b-button></b-col>
+        <b-col cols="auto"><b-button variant="primary" :class="classForStep(0)" v-b-modal.confirm>1: {{ $t("usergroup") }}</b-button></b-col>
         <b-col cols="auto"><b-button variant="primary" :class="classForStep(1)" @click="setStep(1)">2: {{ $t("loginInformation") }}</b-button></b-col>
         <b-col cols="auto"><b-button variant="primary" :class="classForStep(2)" @click="setStep(2)">3: {{ $t("affiliation_contact") }}</b-button></b-col>
         <b-col cols="auto"><b-button variant="primary" :class="classForStep(3)" @click="setStep(3)">4: {{ $t("request") }}</b-button></b-col>
@@ -44,7 +62,7 @@
         <b-col cols="auto"><b-button variant="primary" :class="classForStep(5)" @click="setStep(5)">6: {{ $t("consent") }}</b-button></b-col>
       </b-row>
       <b-row v-else-if="role === 'lab_research'">
-        <b-col cols="auto"><b-button variant="primary" :class="classForStep(0)" @click="setStep(0)">1: {{ $t("usergroup") }}</b-button></b-col>
+        <b-col cols="auto"><b-button variant="primary" :class="classForStep(0)" v-b-modal.confirm>1: {{ $t("usergroup") }}</b-button></b-col>
         <b-col cols="auto"><b-button variant="primary" :class="classForStep(1)" @click="setStep(1)">2: {{ $t("loginInformation") }}</b-button></b-col>
         <b-col cols="auto"><b-button variant="primary" :class="classForStep(2)" @click="setStep(2)">3: {{ $t("affiliation_contact") }}</b-button></b-col>
         <b-col cols="auto"><b-button variant="primary" :class="classForStep(3)" @click="setStep(3)">4: {{ $t("offer") }}</b-button></b-col>
@@ -107,11 +125,15 @@ export default {
       return res;
     },
     setStep(i) {
-      if (i === 0) {
-        this.cache = {}
-      }
       if (this.step < i && this.cache[i] && this.cache[i].indexOf('done') == -1) return;
       this.step = i
+    }
+  },
+  watch: {
+    value(newVal) {
+      if (newVal === 0) {
+        this.cache = {}
+      }
     }
   }
 }
