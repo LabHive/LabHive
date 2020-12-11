c7779bbf371255fb2405c95ee34b7da2d80bb4a0
Philipp Nieting <philipp@nieting.de>
2020-12-11 13:58:56 +0100

    Bugfix

--- src/backend/lib/database/config/options.ts
+++ src/backend/lib/database/config/options.ts
@@ -1,38 +1,11 @@
 import { readFileSync, existsSync } from 'fs'
 import { FILE_PATH } from 'backend/lib/constants'
-import Mail from 'nodemailer/lib/mailer'
-import { EnvVar } from 'backend/lib/EnvVar'
 import { Options } from '../../Options'
 
 class DatabaseOptions extends Options  {
-    private _DISABLE_DISCORD_BOT: EnvVar<boolean>
-    private _ENABLE_MAIL: EnvVar<boolean>
 
     constructor() {
         super()
-
-        // Default values are production configuration
-        this._ENABLE_MAIL = new EnvVar(process.env.ENABLE_MAIL, true)
-        this._DISABLE_DISCORD_BOT = new EnvVar(process.env.DISABLE_DISCORD_BOT, false)
-
-        if (this.ENABLE_MAIL && !existsSync(FILE_PATH.mailConfig)) {
-            console.error("Mail is enabled, but no config exists!")
-            console.error("Set the environment variable ENABLE_MAIL to '0' or 'false' to disable the mail sending feature!")
-            process.exit(1)
-        }
-
-        if (!this.DISABLE_DISCORD_BOT && !existsSync(FILE_PATH.discordBotToken)) {
-            console.error("Discord Bot is enabled, but no discord token could be found!")
-            process.exit(1)
-        }
-    }
-
-    public get ENABLE_MAIL(): boolean {
-        return this._ENABLE_MAIL.value
-    }
-
-    public get DISABLE_DISCORD_BOT(): boolean {
-        return this._DISABLE_DISCORD_BOT.value
     }
 }
 
