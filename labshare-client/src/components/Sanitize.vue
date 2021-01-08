d83de7bf2286100c36aca5dc386db256108918f4
Philipp Nieting <philipp@nieting.de>
2021-01-08 23:40:02 +0100

    Improved Security

---
+++ labshare-client/src/components/Sanitize.vue
@@ -0,0 +1,18 @@
+
+<template>
+  <span>{{ html }}</span>
+</template>
+
+<script>
+
+export default {
+  name: "Sanitize",
+  props: {
+    html: {
+      required: true,
+      type: String
+    }
+  }
+}
+
+</script>
