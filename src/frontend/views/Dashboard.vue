208e6038e9464ebcdbfc1dd7d71b34e16464fafe
Oliver-K-99 <oliver.ka99@web.de>
2021-05-01 16:57:32 +0200

    date is now correct

--- src/frontend/views/Dashboard.vue
+++ src/frontend/views/Dashboard.vue
@@ -47,7 +47,7 @@ export default {
     },
     date: function() {
       
-      let d = new Date(2010, 7, 5);
+      let d = new Date();
       let ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
       let mo = new Intl.DateTimeFormat('en', { month: 'short' }).format(d);
       let da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d);
